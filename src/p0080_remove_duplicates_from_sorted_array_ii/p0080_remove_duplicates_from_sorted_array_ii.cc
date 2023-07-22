4ba018384a455f06213ac9e9f26fc27e6df73711
Lin Chieh <jaycetyle@gmail.com>
2023-07-22 20:44:46 +0800

    P.0080
    Signed-off-by: Lin Chieh <jaycetyle@gmail.com>

---
+++ src/p0080_remove_duplicates_from_sorted_array_ii/p0080_remove_duplicates_from_sorted_array_ii.cc
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int removeDuplicatesII(vector<int>& nums) {
+        int i = 0;
+        int j = 1;
+        int count = 1;
+
+        if (nums.size() <= 1) {
+            return nums.size();
+        }
+
+        for (; j < nums.size(); ++j) {
+            if (nums[i] != nums[j]) {
+                ++i;
+                count = 1;
+                nums[i] = nums[j];
+            } else if (nums[i] == nums[j] && count < 2) {
+                ++i;
+                ++count;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+
+TEST(RemoveDuplicatesTestII, test1) {
+    vector<int> v{1, 1, 1, 2, 2, 3};
+    vector<int> expected{1, 1, 2, 2, 3};
+
+    EXPECT_EQ(5, Solution().removeDuplicatesII(v));
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(expected[i], v[i]);
+    }
+}
+
+TEST(RemoveDuplicatesTestII, test2) {
+    vector<int> v{0, 0, 1, 1, 1, 1, 2, 3, 3};
+    vector<int> expected{0, 0, 1, 1, 2, 3, 3};
+
+    EXPECT_EQ(7, Solution().removeDuplicatesII(v));
+    for (int i = 0; i < 7; ++i) {
+        EXPECT_EQ(expected[i], v[i]);
+    }
+}
