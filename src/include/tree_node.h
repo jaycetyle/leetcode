8925d36612655c9d1e8001a93596bf427f60fb1d
Lin Chieh <jaycetyle@gmail.com>
2023-08-20 10:51:45 +0800

    Move TreeNode struct to one header file
    Signed-off-by: Lin Chieh <jaycetyle@gmail.com>

--- src/include/tree_node.h
+++ src/include/tree_node.h
@@ -1,5 +1,8 @@
 #pragma once
 
+/**
+ * Definition for a binary tree node.
+ */
 struct TreeNode {
   int val;
   TreeNode *left;
--- src/p0894_all_possible_full_binary_trees.cc
+++ src/p0894_all_possible_full_binary_trees.cc
@@ -1,20 +1,10 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include "tree_node.h"
 
 using std::vector;
 
 namespace {
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
 public:
