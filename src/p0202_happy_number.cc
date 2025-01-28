38e8052d1849476a20489d78da2b2c58f222615f
Lin Chieh <jaycetyle@gmail.com>
2025-01-28 12:53:16 +0800

    P.0202
    Signed-off-by: Lin Chieh <jaycetyle@gmail.com>

---
+++ src/p0202_happy_number.cc
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+
+class Solution {
+public:
+    int sqrtSumDigits(int n) {
+        int result = 0;
+
+        while (n) {
+            int rem = n % 10;
+            result += rem * rem;
+            n /= 10;
+        }
+
+        return result;
+    }
+
+    bool isHappy(int n) {
+        bool table[1000] = {0};
+
+        while (true) {
+            n = sqrtSumDigits(n);
+            if (1 == n)
+                return true;
+            if (table[n])
+                return false;
+            table[n] = true;
+        };
+        return false;
+    }
+};
+
+TEST(HappyNumberTest, test1) {
+    EXPECT_TRUE(Solution().isHappy(19));
+    EXPECT_FALSE(Solution().isHappy(2));
+    EXPECT_FALSE(Solution().isHappy(3));
+}
